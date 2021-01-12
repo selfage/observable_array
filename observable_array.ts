916009a9edae94c15d23b6f10a38abad6bdb9209
Yuxuan Xie <teststaybaka@gmail.com>
2021-01-12 23:47:42 +0000

    Migrate ObservableArray to standalone repo.

---
+++ observable_array.ts
@@ -0,0 +1,53 @@
+export class ObservableArray<T> implements Iterable<T> {
+  public onElementChange: (index: number, newValue: T, oldValue: T) => void;
+  private actualArray: T[] = [];
+
+  public constructor() {}
+
+  public get(index: number): T {
+    return this.actualArray[index];
+  }
+
+  public set(index: number, newValue: T): void {
+    let oldValue = this.actualArray[index];
+    this.actualArray[index] = newValue;
+    if (this.onElementChange) {
+      this.onElementChange(index, newValue, oldValue);
+    }
+  }
+
+  public push(...newValues: Array<T>): void {
+    for (let newValue of newValues) {
+      this.actualArray.push(newValue);
+      let index = this.actualArray.length - 1;
+      if (this.onElementChange) {
+        this.onElementChange(index, newValue, undefined);
+      }
+    }
+  }
+
+  public pop(): T {
+    let oldValue = this.actualArray.pop();
+    let index = this.actualArray.length;
+    if (this.onElementChange) {
+      this.onElementChange(index, undefined, oldValue);
+    }
+    return oldValue;
+  }
+
+  get length(): number {
+    return this.actualArray.length;
+  }
+
+  public [Symbol.iterator](): Iterator<T> {
+    return this.actualArray[Symbol.iterator]();
+  }
+
+  public indexOf(value: T): number {
+    return this.actualArray.indexOf(value);
+  }
+
+  public toJSON(): Array<T> {
+    return this.actualArray;
+  }
+}
---
+++ observable_array_test.ts
@@ -0,0 +1,157 @@
+import { ObservableArray } from "./observable_array";
+import { eqObservableArray } from "./observable_array_test_matcher";
+import { Counter } from "@selfage/counter";
+import { assertThat, eq } from "@selfage/test_base/matcher";
+import { TEST_RUNNER } from "@selfage/test_base/runner";
+
+TEST_RUNNER.run({
+  name: "ObservableArrayTest",
+  cases: [
+    {
+      name: "PushAndIterate",
+      execute: () => {
+        // Prepare
+        let arr = new ObservableArray<number>();
+        let counter = new Counter<string>();
+        arr.onElementChange = (index, newValue, oldValue) => {
+          counter.increment("onElementChange");
+          assertThat(index, eq(0), "index");
+          assertThat(newValue, eq(100), "newValue");
+          assertThat(oldValue, eq(undefined), "oldValue");
+        };
+
+        // Execute
+        arr.push(100);
+
+        // Verify
+        assertThat(arr, eqObservableArray([eq(100)]), "arr");
+        assertThat(counter.get("onElementChange"), eq(1), "elementChangeCount");
+
+        // Prepare
+        arr.onElementChange = (index, newValue, oldValue) => {
+          counter.increment("onElementChange");
+          assertThat(index, eq(1), "index");
+          assertThat(newValue, eq(200), "newValue");
+          assertThat(oldValue, eq(undefined), "oldValue");
+        };
+
+        // Execute
+        arr.push(200);
+
+        // Verify
+        assertThat(arr, eqObservableArray([eq(100), eq(200)]), "2nd arr");
+        assertThat(
+          counter.get("onElementChange"),
+          eq(2),
+          "2nd elementChangeCount"
+        );
+
+        // Execute
+        let i = 0;
+        for (let element of arr) {
+          // Verify
+          assertThat(element, eq(arr.get(i)), `${i}th element of arr`);
+          i++;
+        }
+      },
+    },
+    {
+      name: "SetElements",
+      execute: () => {
+        // Prepare
+        let arr = new ObservableArray<number>();
+        arr.push(100, 200);
+        let counter = new Counter<string>();
+        arr.onElementChange = (index, newValue, oldValue) => {
+          counter.increment("onElementChange");
+          assertThat(index, eq(0), "index");
+          assertThat(newValue, eq(1), "newValue");
+          assertThat(oldValue, eq(100), "oldValue");
+        };
+
+        // Execute
+        arr.set(0, 1);
+
+        // Verify
+        assertThat(arr, eqObservableArray([eq(1), eq(200)]), "arr");
+        assertThat(counter.get("onElementChange"), eq(1), "elementChangeCount");
+
+        // Prepare
+        arr.onElementChange = (index, newValue, oldValue) => {
+          counter.increment("onElementChange");
+          assertThat(index, eq(1), "index");
+          assertThat(newValue, eq(2), "newValue");
+          assertThat(oldValue, eq(200), "oldValue");
+        };
+
+        // Execute
+        arr.set(1, 2);
+
+        // Verify
+        assertThat(arr, eqObservableArray([eq(1), eq(2)]), "2nd arr");
+        assertThat(
+          counter.get("onElementChange"),
+          eq(2),
+          "2nd elementChangeCount"
+        );
+      },
+    },
+    {
+      name: "PopElements",
+      execute: () => {
+        // Prepare
+        let arr = new ObservableArray<number>();
+        arr.push(100, 200);
+        let counter = new Counter<string>();
+        arr.onElementChange = (index, newValue, oldValue) => {
+          counter.increment("onElementChange");
+          assertThat(index, eq(1), "index");
+          assertThat(newValue, eq(undefined), "newValue");
+          assertThat(oldValue, eq(200), "oldValue");
+        };
+
+        // Execute
+        let value = arr.pop();
+
+        // Verify
+        assertThat(arr, eqObservableArray([eq(100)]), "arr");
+        assertThat(value, eq(200), "value");
+        assertThat(counter.get("onElementChange"), eq(1), "elementChangeCount");
+
+        // Prepare
+        arr.onElementChange = (index, newValue, oldValue) => {
+          counter.increment("onElementChange");
+          assertThat(index, eq(0), "index");
+          assertThat(newValue, eq(undefined), "newValue");
+          assertThat(oldValue, eq(100), "oldValue");
+        };
+
+        // Execute
+        value = arr.pop();
+
+        // Verify
+        assertThat(arr, eqObservableArray([]), "2nd arr");
+        assertThat(value, eq(100), "2nd value");
+        assertThat(
+          counter.get("onElementChange"),
+          eq(2),
+          "2nd elementChangeCount"
+        );
+      },
+    },
+    {
+      name: "JsonStringify",
+      execute: () => {
+        // Prepare
+        let arr = new ObservableArray<number>();
+        arr.push(100, 200);
+
+        // Execute
+        let serailzied = JSON.stringify(arr);
+
+        // Verify
+        assertThat(serailzied, eq("[100,200]"), "serailzied");
+      },
+    },
+  ],
+});
