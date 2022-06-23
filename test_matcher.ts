40566b2c99715110390d8fa6d2b79840289a3666
Yuxuan Xie <teststaybaka@gmail.com>
2022-06-23 05:53:22 +0000

    Check null. Release

--- test_matcher.ts
+++ test_matcher.ts
@@ -1,5 +1,5 @@
 import { ObservableArray } from "./observable_array";
-import { MatchFn, assertThat, eq } from "@selfage/test_matcher";
+import { MatchFn, assertThat, eq, assert } from "@selfage/test_matcher";
 
 export function eqObservableArray<T>(
   expected?: Array<MatchFn<T>>
@@ -9,6 +9,7 @@ export function eqObservableArray<T>(
       assertThat(actual, eq(undefined), "nullity");
       return;
     }
+    assert(Boolean(actual), `to not be null`, `null`);
     assertThat(actual.length, eq(expected.length), `array length`);
     for (let i = 0; i < actual.length; i++) {
       assertThat(actual.get(i), expected[i], `${i}th element`);
